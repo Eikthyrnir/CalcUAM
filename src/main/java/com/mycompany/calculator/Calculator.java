34193033a9bb1f1e4c387d995f7cf25a546ed67e
Eikthyrnir <timbarvenov@gmail.com>
2023-06-08 00:28:16 +0200

    tests without mutations

--- src/main/java/com/mycompany/calculator/Calculator.java
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -2,71 +2,71 @@ package com.mycompany.calculator;
 
 public class Calculator {
 
-       
+
     public double powerTwoNumbers(double a, double b) {
         return Math.pow(a, b);
     }
-    
-    public double sqrt (double a) {
+
+    public double sqrt(double a) {
         return Math.sqrt(a);
     }
-    
-    
+
+
     public double CalculateSquareArea(int a) {
-        return a*a;
+        return a * a;
     }
-    
+
     public double CalculateRectArea(int a, int b) {
-        return a*b;
+        return a * b;
     }
-    
+
     public int CalculateSquareCirc(int a) {
-        return 4*a;
+        return 4 * a;
     }
-    
+
     public int CalculateRectCirc(int a, int b) {
-        return 2*a+2*b;
+        return 2 * a + 2 * b;
     }
-    
-     public double CalculateTriangleArea(int a, int h) {
-        return (a*h)/2;
+
+    public double CalculateTriangleArea(int a, int h) {
+        return (a * h) / 2;
     }
-     
-      public double CalculateTriangleCirc(int a, int b, int c) {
-        return a+b+c;
+
+    public double CalculateTriangleCirc(int a, int b, int c) {
+        return a + b + c;
     }
-      
-    public int convertCelsiusToFarenheit (int celcius) {
-        return (9/5)*celcius+32;
+
+    public int convertCelsiusToFarenheit(int celcius) {
+        return (9 / 5) * celcius + 32;
     }
-    
-    public int convertFarenheitToCelsius (int farenheit) {
-        return (5/9)*(farenheit-32);
+
+    public int convertFarenheitToCelsius(int farenheit) {
+        return (5 / 9) * (farenheit - 32);
     }
-    
+
     public double convertFromMphToKph(int mph) {
         return mph * 1.609344;
     }
-    
+
     public int factorial(int i) {
         if (i < 1) return 1;
-        else return i * factorial (i - 1);
+        else return i * factorial(i - 1);
     }
-    
-    public int fibbonacci (int i) {
+
+    public int fibbonacci(int i) {
         if (i == 0 || i == 1) return i;
-        return fibbonacci (i-1) + fibbonacci (i-2);
+        return fibbonacci(i - 1) + fibbonacci(i - 2);
     }
-    
+
     public int findGCD(int a, int b) {
         if (b == 0) return a;
-        return findGCD(b, a%b);
+        return findGCD(b, a % b);
     }
-    
+
     public int findLCM(int a, int b) {
-        return a*b/findGCD(a,b);
+        return a * b / findGCD(a, b);
     }
-    
+
     public boolean isPrime(int n) {
         if (n <= 1) {
             return false;
---
+++ src/test/java/com/mycompany/calculator/CalculatorTest.java
@@ -0,0 +1,168 @@
+package com.mycompany.calculator;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    private static Calculator calculator;
+
+    @BeforeAll
+    static void init() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void addTwoNumbersTest() {
+        assertEquals(8, calculator.CalculateRectCirc(2, 2));
+    }
+
+    @Test
+    public void testPowerTwoNumbers() {
+        assertEquals(
+                Math.pow(2, 3),
+                calculator.powerTwoNumbers(2, 3)
+        );
+    }
+
+    @Test
+    public void testSqrt() {
+        assertEquals(
+                Math.sqrt(3),
+                calculator.sqrt(3)
+        );
+    }
+
+    @Test
+    public void testCalculateSquareArea() {
+        assertEquals(
+                3 * 3,
+                calculator.CalculateSquareArea(3)
+        );
+    }
+
+    @Test
+    public void testCalculateRectArea() {
+        assertEquals(
+                2 * 3,
+                calculator.CalculateRectArea(2, 3)
+        );
+    }
+
+    @Test
+    public void testCalculateSquareCirc() {
+        assertEquals(
+                4 * 3,
+                calculator.CalculateSquareCirc(3)
+        );
+    }
+
+    @Test
+    public void testCalculateRectCirc() {
+        assertEquals(
+                2 * 3 + 2 * 2,
+                calculator.CalculateRectCirc(3, 2)
+        );
+    }
+
+    @Test
+    public void testCalculateTriangleArea() {
+        assertEquals(
+                3 * 2 * 0.5,
+                calculator.CalculateTriangleArea(3, 2)
+        );
+        assertEquals(
+                3 * 3 * 0.5,
+                calculator.CalculateTriangleArea(3, 3)
+        );
+    }
+
+    @Test
+    public void testCalculateTriangleCirc() {
+        assertEquals(
+                3 + 2 + 4,
+                calculator.CalculateTriangleCirc(3, 2, 4)
+        );
+    }
+
+    @Test
+    public void testConvertCelsiusToFarenheit() {
+        int celcius = 36;
+        assertEquals(
+                (9 / 5.0) * celcius + 32,
+                calculator.convertCelsiusToFarenheit(celcius)
+        );
+    }
+
+    @Test
+    public void testConvertFarenheitToCelsius() {
+        int farenheit = 36;
+        assertEquals(
+                (5 / 9.0) * (farenheit - 32),
+                calculator.convertFarenheitToCelsius(farenheit)
+        );
+    }
+
+    @Test
+    public void testConvertFromMphToKph() {
+        int mph = 100;
+        assertEquals(
+                mph * 1.609344,
+                calculator.convertFromMphToKph(mph)
+        );
+    }
+
+    @Test
+    public void testFactorial() {
+        assertEquals(
+                2 * 3,
+                calculator.factorial(3)
+        );
+    }
+
+    @Test
+    public void testFibbonacci() {
+        assertEquals(
+                2,
+                calculator.fibbonacci(3)
+        );
+    }
+
+    @Test
+    public void testFindGCD() {
+        assertEquals(
+                4,
+                calculator.findGCD(12, 28)
+        );
+        assertEquals(
+                calculator.findGCD(28, 12),
+                calculator.findGCD(12, 28)
+        );
+    }
+
+    @Test
+    public void testFindLCM() {
+        assertEquals(
+                3 * 4 * 7,
+                calculator.findLCM(12, 28)
+        );
+    }
+
+    @Test
+    public void correctLCMUsingGCD() {
+        assertEquals(
+                Math.abs(12 * 28) / calculator.findGCD(12, 28),
+                calculator.findLCM(12, 28)
+        );
+    }
+
+    @Test
+    public void testIsPrime() {
+        assertTrue(calculator.isPrime(2));
+        assertTrue(calculator.isPrime(11));
+        assertFalse(calculator.isPrime(1));
+        assertFalse(calculator.isPrime(12));
+    }
+}
